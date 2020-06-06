38437bcbdccbf76b39b79889d3e2314140289192
Sergey Mironov <66295155+FeagurSE@users.noreply.github.com>
2020-06-06 23:48:53 +0300

    Add files via upload

--- AltayLOM/leaders/models.py
+++ AltayLOM/leaders/models.py
@@ -9,13 +9,16 @@ class Profile(models.Model):
 
     name = models.CharField(verbose_name='Имя', max_length=200, null=True)
     surname = models.CharField(verbose_name='Фамилия', max_length=200, null=True)
-    second_Name = models.CharField(verbose_name='Отчество', max_length=200, null=True)
+    second_Name = models.CharField(verbose_name='Отчество', max_length=200)
     voices = models.IntegerField(verbose_name='Голоса', null=True)
-    place = models.CharField(verbose_name='Город (Район)', max_length=200, null=True)
+    place = models.CharField(verbose_name='Город (Район)', max_length=200)
     email = models.EmailField(verbose_name='Email', null=True)
-    phone = models.CharField(verbose_name='Телефон', max_length=200, null=True)
-    photo = models.ImageField(verbose_name='Фото', upload_to='images', null=True)
+    phone = models.CharField(verbose_name='Телефон', max_length=200)
+    photo = models.ImageField(verbose_name='Фото', upload_to='images')
     voice_status = models.BooleanField(verbose_name='Мой голос')
+    leader_status = models.BooleanField(verbose_name='Лидер общественного мнения')
+    biography = models.TextField(verbose_name='О себе')
+    social_network = models.URLField(verbose_name='Социальные сети')
 
 
 # Модель заявки
@@ -30,3 +33,14 @@ class Action(models.Model):
     date_init = models.DateTimeField(verbose_name='Время подачи', null=True)
     status = models.BooleanField(verbose_name='Статус исполнения')
     date_exec = models.DateTimeField(verbose_name='Время Исполнения', null=True)
+
+
+# Модель Рецензии
+class Review(models.Model):
+    class Meta:
+        verbose_name = 'Рецензия'
+        verbose_name_plural = 'Рецензии'
+
+    title = models.CharField(verbose_name='Рецензия', max_length=200, null=True)
+    description = models.CharField(verbose_name='Содержание', max_length=200, null=True)
+    date_init = models.DateTimeField(verbose_name='Время подачи', null=True)
