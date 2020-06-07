68b3e99ecdb457f3341a0eb4e6b39a62808a2ccc
Sergey Mironov <66295155+FeagurSE@users.noreply.github.com>
2020-06-07 08:35:49 +0300

    Add files via upload

--- AltayLOM/leaders/models.py
+++ AltayLOM/leaders/models.py
@@ -10,12 +10,10 @@ class Profile(models.Model):
     name = models.CharField(verbose_name='Имя', max_length=200, null=True)
     surname = models.CharField(verbose_name='Фамилия', max_length=200, null=True)
     second_Name = models.CharField(verbose_name='Отчество', max_length=200)
-    voices = models.IntegerField(verbose_name='Голоса', null=True)
     place = models.CharField(verbose_name='Город (Район)', max_length=200)
     email = models.EmailField(verbose_name='Email', null=True)
     phone = models.CharField(verbose_name='Телефон', max_length=200)
     photo = models.ImageField(verbose_name='Фото', upload_to='images')
-    voice_status = models.BooleanField(verbose_name='Мой голос')
     leader_status = models.BooleanField(verbose_name='Лидер общественного мнения')
     biography = models.TextField(verbose_name='О себе')
     social_network = models.URLField(verbose_name='Социальные сети')
@@ -44,3 +42,15 @@ class Review(models.Model):
     title = models.CharField(verbose_name='Рецензия', max_length=200, null=True)
     description = models.CharField(verbose_name='Содержание', max_length=200, null=True)
     date_init = models.DateTimeField(verbose_name='Время подачи', null=True)
+
+
+# Модель Голоса
+class Voises(models.Model):
+    class Meta:
+        verbose_name = 'Голос'
+        verbose_name_plural = 'Голоса'
+
+    voices = models.IntegerField(verbose_name='Голоса', null=True)
+    voice_status = models.BooleanField(verbose_name='Мой голос')
+    voices_negative = models.IntegerField(verbose_name='Отозванные голоса')
+    voting_positive = models.IntegerField(verbose_name='Голосований в пользу лидера')
